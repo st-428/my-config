7d3b71f6f8582bbb4d2819c1606187b5f30527e3
st-428 <unusfrank@gmail.com>
2026-03-25 21:08:16 +0800

    2026-03-25

--- niri/autostart.sh
+++ niri/autostart.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 exec >/tmp/niri-autostart.log 2>&1
+export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+export XDG_RUNTIME_DIR="/run/user/$(id -u)"
 
+sleep 0.1
 /usr/bin/alacritty --title "niri-main" &
 sleep 0.1
 /usr/bin/alacritty --title "niri-clock" -e tty-clock -c -b -s -C 6 &
@@ -8,11 +11,11 @@ sleep 0.1
 /usr/bin/alacritty --title "niri-htop" -e htop &
 sleep 0.1
 /usr/bin/alacritty --title "niri-nvtop" -e nvtop &
-sleep 0.1
+sleep 0.2
 niri msg action focus-column-first
-sleep 0.1
+sleep 0.2
 niri msg action consume-window-into-column
-sleep 0.1
+sleep 0.2
 niri msg action focus-column-right
-sleep 0.1
+sleep 0.2
 niri msg action consume-window-into-column
