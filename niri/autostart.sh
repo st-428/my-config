0a15cc58d331e831c2ec938e88aeb14e451d5b2d
st-428 <unusfrank@gmail.com>
2026-03-25 21:41:09 +0800

    htop config

--- niri/autostart.sh
+++ niri/autostart.sh
@@ -3,13 +3,13 @@ exec >/tmp/niri-autostart.log 2>&1
 export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 export XDG_RUNTIME_DIR="/run/user/$(id -u)"
 
-sleep 0.1
+sleep 0.2
 /usr/bin/alacritty --title "niri-main" &
-sleep 0.1
+sleep 0.2
 /usr/bin/alacritty --title "niri-clock" -e tty-clock -c -b -s -C 6 &
-sleep 0.1
+sleep 0.2
 /usr/bin/alacritty --title "niri-htop" -e htop &
-sleep 0.1
+sleep 0.2
 /usr/bin/alacritty --title "niri-nvtop" -e nvtop &
 sleep 0.2
 niri msg action focus-column-first
